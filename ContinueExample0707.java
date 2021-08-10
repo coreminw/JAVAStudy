2ebcd00436cc94ca5e5ba80bbe1256c627556d5f
김민욱 <coreminw@naver.com>
2021-08-10 20:06:38 +0900

    continue example

--- ContinueExample0707.java
+++ ContinueExample0707.java
@@ -2,11 +2,10 @@ public class ContinueExample0707 {
     public static void main(String args[]){
         int i;//i는 변수..?ㅎ sum은 짝수의 합
         int sum=0;
-        for(i=1; i<=100; i++){
-            if(i%2==1) {
+        for(i=1; i<=100; i++) {
+            if (i % 2 == 1) {
                 continue;
-            }
-            else {
+            } else {
                 sum += i;
             }
         }
