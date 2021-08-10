3d0fd6afb3d740b6a32888d9b2c2e147d1fe6c95
김민욱 <coreminw@naver.com>
2021-08-10 19:54:28 +0900

    4x4배열

---
+++ ArrayTest20710.java
@@ -0,0 +1,17 @@
+public class ArrayTest20710 {
+    public static void main(String args[]){
+        int[][] intArray= new int[4][4];
+        int count=0;
+        int i=0;
+        int j=0;
+
+        while
+
+        for( i=0; i<intArray.length; i++){
+            for(j=0; j<intArray[i].length; j++){
+                System.out.print(intArray[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+}
