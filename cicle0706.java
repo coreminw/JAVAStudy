45860bee723728018425761bc6b1092c9e36bdf0
김민욱 <coreminw@naver.com>
2021-08-10 19:58:12 +0900

    원의 중심과 반지름

---
+++ cicle0706.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class cicle0706 {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("원의 중심과 반지름 입력: ");
+        int num1= sc.nextInt();
+        int num2= sc.nextInt();
+        double num3= sc.nextDouble();
+
+    }
+}
