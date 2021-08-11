edca42818345ea68b145455386c2db1d1c89bef4
김민욱 <coreminw@naver.com>
2021-08-11 23:04:32 +0900

    나눗셈

---
+++ ExceptionExample0708.java
@@ -0,0 +1,22 @@
+//import java.util.Scanner;
+//
+//public class ExceptionExample0708 {
+//    public static void main(String args[]){
+//        Scanner sc = new Scanner(System.in);
+//
+//        int a=0;//나뉨수
+//        int b=0;//나눗수
+//
+//        System.out.println("나뉨 수 입력");
+//        a= sc.nextInt();
+//        System.out.println("나눗 수 입력");
+//        b= sc.nextInt();
+//
+//        try {
+//            System.out.println(a+"를"+b+"로 나누면"+a/b);
+//        }catch {
+//            System.out.println("0으로 나누기 불가");
+//        }
+//
+//    }
+//}
