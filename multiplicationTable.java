d1ba1ae25d373f29f310bcadd1b132385bdca2f3
김민욱 <coreminw@naver.com>
2021-09-07 21:12:56 +0900

    구구단

---
+++ multiplicationTable.java
@@ -0,0 +1,13 @@
+public class multiplicationTable {
+    public static void main(String args[]){
+        print();
+    }
+    public static void print(){
+        for(int i=2; i<10; i++){
+            System.out.println(i+"단");
+            for(int j=1; j<10; j++){
+                System.out.println(i+"*"+j+"="+ i*j);
+            }
+        }
+    }
+}
